01ed3e303f3fde07bcc3d0a76cad8fa61096d93b
Mark Maglana <mmaglana@gmail.com>
2014-01-30 00:44:07 -0800

    Exercise 3

---
+++ ex03/ex3.py
@@ -0,0 +1,41 @@
+# Prints a string
+print "I will now count my chickens:"
+
+# Prints a string and the result of adding 25 and (30/6)
+print "Hens", 25 + 30 / 6
+
+# Prints string and the result of 100 - ((25 * 3) % 4)
+print "Roosters", 100 - 25 * 3 % 4
+
+# Prints a string
+print "Now I will count the eggs:"
+
+# Prints the result of (3+2+1-5)+(0)-(0)+6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
+
+# Prints a string
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Prints the result of the less-than comparison
+print 3 + 2 < 5 - 7
+
+# Prints a string and the result of the addition
+print "What is 3 + 2?", 3 + 2
+
+# Prints a string and the result of a subtraction
+print "What is 5 - 7?", 5 - 7
+
+# Prints a string
+print "Oh, that's why it's False."
+
+# Prints a string
+print "How about some more."
+
+# Prints a string and the result of a 'greater-than' comparison
+print "Is it greater?", 5 > -2
+
+# Prints a string and whether 5 is greater than or equal to -2
+print "Is it greater or equal?", 5 >= -2
+
+# Prints a string and whether 5 is less than or equal to -2
+print "Is it less or equal?", 5 <= -2
---
+++ ex03/ex3b.py
@@ -0,0 +1,7 @@
+# Find something you need to calculate and write a new .py file that does it.
+
+print "How long does it take to drive from Redondo to Pleasanton at 55mph?"
+
+print "The distance is 357 miles."
+
+print "It takes", 357.0 / 55, "hours. So drive faster, and skip the weighing scales!"
