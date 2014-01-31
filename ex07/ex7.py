030b72a8ccdf2c2758c0435efa2f6f33187931e5
Mark Maglana <mmaglana@gmail.com>
2014-01-30 21:47:20 -0800

    Exercise 7

---
+++ ex07/ex7.py
@@ -0,0 +1,26 @@
+# These just print strings
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+
+# This prints a period 10 times
+print "." * 10
+
+# Assign single-character strings to variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# The comma at the end of a print sequence means
+# the next printed string(s) will be shown on the same line
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
